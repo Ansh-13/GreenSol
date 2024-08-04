4aa3c46254b334e1b6d3142188563db0f05a035c
Ansh-13 <liveansh27@gmail.com>
2024-08-04 15:31:09 +0530

    Auth function Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import { setDoc, doc } from "firebase/firestore";
 import { useState } from "react";
 import { redirect } from "next/navigation";
 import { useRouter } from "next/navigation";
+import SignOut from "./signout/page";
+
 export default function Home() {
   const [Annual_Electricity_Consumption, Set_Annual_Electricity_Consumption] =
     useState("");
@@ -15,21 +17,19 @@ export default function Home() {
   const [Requirement, Set_requirment] = useState("");
   const [user, loading, error] = useAuthState(auth);
 
-  //const { Data, setData } = useContext(RoiContext);
+  if (!user) {
+    redirect("/signup");
+  }
+
   const router = useRouter();
 
   const uid = user?.uid;
   uid?.toString();
-  //console.log(typeof uid);
 
   const handleChange = (e: any) => {
     e.preventDefault();
     Set_requirment(e.target.value);
   };
-
-  //console.log(Requirement);
-
-  //console.log(user?.uid);
 
   const handleSumit = async (e: any) => {
     e.preventDefault();
@@ -119,6 +119,7 @@ export default function Home() {
     // console.error("Error adding document: ", error);
     // }
   };
+
   if (error) {
     return (
       <div>
@@ -138,121 +139,126 @@ export default function Home() {
   return (
     <div className="w-auto h-auto flex justify-center items-center relative">
       {user ? (
-        <div className="w-[30rem] h-[30rem] rounded-lg flex justify-center absolute top-20 bg-[#228b22]">
-          <form onSubmit={handleSumit} className="my-5">
-            <div className="sm:col-span-2">
-              <label className="block text-sm font-medium leading-6 text-white">
-                Annual Electricity unit Consumption
-              </label>
-              <div className="mt-2">
-                <input
-                  id="Annual Electricity unit Consumption"
-                  name="Annual Electricity unit Consumption"
-                  type="text"
-                  className="block w-full focus:outline-0 px-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  onChange={(e) =>
-                    Set_Annual_Electricity_Consumption(e.target.value)
-                  }
-                />
-              </div>
-            </div>
-
-            <div className="sm:col-span-2 my-3">
-              <label className="block text-sm font-medium leading-6 text-white">
-                Annual Electricity Bill / cost
-              </label>
-              <div className="mt-2">
-                <input
-                  id="Annual_Electricity_Costn"
-                  name="Annual_Electricity_Cost"
-                  type="text"
-                  className="block w-full focus:outline-0 px-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  onChange={(e) => Set_Annual_Electricity_Cost(e.target.value)}
-                />
-              </div>
-            </div>
-            <div className="sm:col-span-2">
-              <label className="block text-sm font-medium leading-6 text-white">
-                Shadow Free Space
-              </label>
-              <div className="mt-2">
-                <input
-                  id="Shadow_Free_Space"
-                  name="Shadow_Free_Space"
-                  type="text"
-                  className="block w-full focus:outline-0 px-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  onChange={(e) => Set_Shadow_Free_Space(e.target.value)}
-                />
-              </div>
-            </div>
-            <div className="mt-4 space-y-10">
-              <fieldset id="Requirement_Type">
-                <legend className="text-md font-semibold leading-6 text-white">
-                  Requirement Type
-                </legend>
-                <div className="mt-6 space-y-2">
-                  <div className="flex items-center gap-x-3">
-                    <input
-                      id="save_money"
-                      name="push-notifications"
-                      type="radio"
-                      value={"OnGird"}
-                      onChange={handleChange}
-                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                    />
-                    <label
-                      htmlFor="save_money"
-                      className="block text-sm font-medium leading-6 text-white"
-                    >
-                      Save Money
-                    </label>
-                  </div>
-                  <div className="flex items-center gap-x-3">
-                    <input
-                      id="electricity_store"
-                      name="push-notifications"
-                      type="radio"
-                      value={"OffGrid"}
-                      onChange={handleChange}
-                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                    />
-                    <label
-                      htmlFor="electricity_store"
-                      className="block text-sm font-medium leading-6 text-white"
-                    >
-                      Want to Store Electricity
-                    </label>
-                  </div>
-                  <div className="flex items-center gap-x-3">
-                    <input
-                      id="all_time_electricity"
-                      name="push-notifications"
-                      type="radio"
-                      value={"Hybrid"}
-                      onChange={handleChange}
-                      className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                    />
-                    <label
-                      htmlFor="all_time_electricity"
-                      className="block text-sm font-medium leading-6 text-white"
-                    >
-                      24/7 Electricity
-                    </label>
-                  </div>
+        <>
+          <SignOut />
+          <div className="w-[30rem] h-[30rem] rounded-lg flex justify-center absolute top-20 bg-[#228b22]">
+            <form onSubmit={handleSumit} className="my-5">
+              <div className="sm:col-span-2">
+                <label className="block text-sm font-medium leading-6 text-white">
+                  Annual Electricity unit Consumption
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="Annual Electricity unit Consumption"
+                    name="Annual Electricity unit Consumption"
+                    type="text"
+                    className="block w-full focus:outline-0 px-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    onChange={(e) =>
+                      Set_Annual_Electricity_Consumption(e.target.value)
+                    }
+                  />
                 </div>
-              </fieldset>
-            </div>
-            <div className="mt-6 flex items-center  gap-x-6">
-              <button
-                type="submit"
-                className="rounded-md bg-[#62fcaf] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                onClick={() => router.push("/details")}
-              >
-                Save
-              </button>
-            </div>
-          </form>
-        </div>
+              </div>
+
+              <div className="sm:col-span-2 my-3">
+                <label className="block text-sm font-medium leading-6 text-white">
+                  Annual Electricity Bill / cost
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="Annual_Electricity_Costn"
+                    name="Annual_Electricity_Cost"
+                    type="text"
+                    className="block w-full focus:outline-0 px-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    onChange={(e) =>
+                      Set_Annual_Electricity_Cost(e.target.value)
+                    }
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label className="block text-sm font-medium leading-6 text-white">
+                  Shadow Free Space
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="Shadow_Free_Space"
+                    name="Shadow_Free_Space"
+                    type="text"
+                    className="block w-full focus:outline-0 px-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    onChange={(e) => Set_Shadow_Free_Space(e.target.value)}
+                  />
+                </div>
+              </div>
+              <div className="mt-4 space-y-10">
+                <fieldset id="Requirement_Type">
+                  <legend className="text-md font-semibold leading-6 text-white">
+                    Requirement Type
+                  </legend>
+                  <div className="mt-6 space-y-2">
+                    <div className="flex items-center gap-x-3">
+                      <input
+                        id="save_money"
+                        name="push-notifications"
+                        type="radio"
+                        value={"OnGird"}
+                        onChange={handleChange}
+                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      />
+                      <label
+                        htmlFor="save_money"
+                        className="block text-sm font-medium leading-6 text-white"
+                      >
+                        Save Money
+                      </label>
+                    </div>
+                    <div className="flex items-center gap-x-3">
+                      <input
+                        id="electricity_store"
+                        name="push-notifications"
+                        type="radio"
+                        value={"OffGrid"}
+                        onChange={handleChange}
+                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      />
+                      <label
+                        htmlFor="electricity_store"
+                        className="block text-sm font-medium leading-6 text-white"
+                      >
+                        Want to Store Electricity
+                      </label>
+                    </div>
+                    <div className="flex items-center gap-x-3">
+                      <input
+                        id="all_time_electricity"
+                        name="push-notifications"
+                        type="radio"
+                        value={"Hybrid"}
+                        onChange={handleChange}
+                        className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                      />
+                      <label
+                        htmlFor="all_time_electricity"
+                        className="block text-sm font-medium leading-6 text-white"
+                      >
+                        24/7 Electricity
+                      </label>
+                    </div>
+                  </div>
+                </fieldset>
+              </div>
+              <div className="mt-6 flex items-center  gap-x-6">
+                <button
+                  type="submit"
+                  className="rounded-md bg-[#62fcaf] px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  onClick={() => router.push("/details")}
+                >
+                  Save
+                </button>
+              </div>
+            </form>
+          </div>
+        </>
       ) : (
         <p>user is not found</p>
       )}
